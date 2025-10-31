2efa4842e86796c52c6f5e77539263c50cc9c433
pengfeick <aadgof3111@outlook.com>
2025-10-31 13:23:25 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const methToken = await deploy("ERC7984ETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const staking = await deploy("METHStaking", {
+    from: deployer,
+    args: [methToken.address],
+    log: true,
+  });
+
+  console.log(`ERC7984ETH contract deployed at ${methToken.address}`);
+  console.log(`METHStaking contract deployed at ${staking.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_meth";
+func.tags = ["METH", "STAKING"];
